76c938182a8e08b08c26556e07f53e57eff03b46
anikan <anikan33@gmail.com>
2016-05-21 11:15:39 -0700

    Added bin system

---
+++ Assets/Scripts/BinScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum BinType { Trash, Recycle, Yard, Battery }
+
+
+public class BinScript : MonoBehaviour {
+
+    public BinType type;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/OverallStatus.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverallStatus : MonoBehaviour {
+
+    public static GameObject playerCamera;
+    public static GameObject textBubblePrefab;
+    public GameObject textBubblePrefabInput;
+
+    void Awake()
+    {
+        playerCamera = Camera.main.gameObject;
+        textBubblePrefab = textBubblePrefabInput;
+        Debug.Log(textBubblePrefab);
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
